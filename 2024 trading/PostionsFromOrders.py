4829a9fe9bdd965e9f25ea7e975991296f5c4a06
Himanshu Sharma <himanshu4529.pec@gmail.com>
2025-02-24 14:01:24 +0530

    Code optimized to use a common utility function.

--- 2024 trading/PostionsFromOrders.py
+++ 2024 trading/PostionsFromOrders.py
@@ -1,33 +1,11 @@
 import pandas as pd
-import datetime as dt
-from kiteconnect import KiteConnect
-
-
-KITE_API_KEY = '453dipfh64qcl484'
-KITE_ACCESS_CODE = 'PEOyxkmvl0aO7oD0N34gRCOhT4AniPbo'
-MARKET_START_TIME = dt.time (9, 15, 0, 100)
-MARKET_END_TIME = dt.time (15, 25, 0)
-TRADE_START_TIME = dt.time (9, 15, 30)
-
-
-def intialize_kite_api():
-    kite = KiteConnect (api_key=KITE_API_KEY)
-
-    try:
-
-        kite.set_access_token(KITE_ACCESS_CODE)
-    except Exception as e:
-        print("Authentication failed", str(e))
-        raise
-
-    return kite
-
+import OptionTradeUtils as oUtils
 
 if __name__ == '__main__':
 
-    kite = intialize_kite_api()
+    kite = oUtils.intialize_kite_api()
 
-    orders = kite.orders();
+    orders = kite.orders()
 
     # Create pandas DataFrame from the list of orders
     df = pd.DataFrame(orders)
